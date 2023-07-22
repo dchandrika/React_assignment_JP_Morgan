cb30cbc9373e0d1294bf0343df1aaf34eb7dccfe
dchandrika <desai.chandrika@gmail.com>
2023-07-22 13:50:22 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import TableComponent from "./TableComponent";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Financial instruments</h1>
+      <TableComponent />
+    </div>
+  );
+}
---
+++ src/TableComponent.jsx
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from "react";
+import jsonData from "./mockData.json";
+
+const TableComponent = () => {
+  const [data, setData] = useState([]);
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [sortingCriteria, setSortingCriteria] = useState("");
+
+  // Set the fetched JSON data from the mockData.json
+  useEffect(() => {
+    let apiRes = fetch("https://dummyjson.com/products").then((response) =>
+      response.json()
+    );
+    apiRes.then((data) => setData(jsonData));
+  }, []);
+
+  const toggleAssetSortOrder = () => {
+    const newSortOrder = sortOrder === "asc" ? "desc" : "asc";
+    setSortOrder(newSortOrder);
+
+    let sortedData = [...data];
+
+    sortedData.sort((a, b) => {
+      if (sortOrder === "asc") {
+        if (a.assetClass < b.assetClass) return -1;
+        if (a.assetClass > b.assetClass) return 1;
+      } else {
+        if (a.assetClass < b.assetClass) return 1;
+        if (a.assetClass > b.assetClass) return -1;
+      }
+      return 0;
+    });
+
+    setData(sortedData);
+  };
+
+  const sortData = (criteria) => {
+    setSortingCriteria(criteria);
+    let sortedData = [...jsonData];
+
+    if (criteria === "priceDescending") {
+      sortedData.sort((a, b) => b.price - a.price);
+    } else if (criteria === "tickerAscending") {
+      sortedData.sort((a, b) => a.ticker.localeCompare(b.ticker));
+    }
+
+    setData(sortedData);
+  };
+
+  const getRowClassName = (assetClass) => {
+    switch (assetClass) {
+      case "Macro":
+        return "row-red";
+      case "Equities":
+        return "row-blue";
+      case "Credit":
+        return "row-green";
+      default:
+        return "";
+    }
+  };
+
+  return (
+    <div>
+      <div className="reset-order">
+        <button onClick={() => setData(jsonData)} className="btn">
+          Reset Sorting
+        </button>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>
+              <div>Ticker</div>
+              <button
+                onClick={() => sortData("tickerAscending")}
+                className="btn"
+              >
+                Sort (Alpha)
+              </button>
+            </th>
+            <th>
+              <div>Price</div>
+              <button
+                onClick={() => sortData("priceDescending")}
+                className="btn"
+              >
+                Sort (Desc)
+              </button>
+            </th>
+            <th>
+              <div>Asset Class</div>
+              <button onClick={toggleAssetSortOrder} className="btn">
+                {sortOrder === "asc" ? "Sort (Asc)" : "Sort (Desc)"}
+              </button>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index} className={getRowClassName(item.assetClass)}>
+              <td>{item.ticker}</td>
+              <td style={{ color: item.price >= 0 ? "black" : "red" }}>
+                {item.price}
+              </td>
+              <td>{item.assetClass}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TableComponent;
